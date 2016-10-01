ed79536bf21d0d65dd782034e9c6b048d828bc50
Joseph Cumines <joeycumines@gmail.com>
2016-10-01 17:34:01 +1000

    Todo Document and getters/setters

---
+++ src/AppBundle/Document/Todo.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ * Models a To do object, owned by a user.
+ */
+
+namespace AppBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document
+ */
+class Todo
+{
+    /**
+     * @MongoDB\Id(strategy="auto")
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\Field(type="object_id")
+     */
+    protected $userId;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $title;
+
+    /**
+     * @MongoDB\Field(type="boolean")
+     */
+    protected $completed;
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param object_id $userId
+     * @return $this
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return object_id $userId
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string $title
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set completed
+     *
+     * @param boolean $completed
+     * @return $this
+     */
+    public function setCompleted($completed)
+    {
+        $this->completed = $completed;
+        return $this;
+    }
+
+    /**
+     * Get completed
+     *
+     * @return boolean $completed
+     */
+    public function getCompleted()
+    {
+        return $this->completed;
+    }
+}
